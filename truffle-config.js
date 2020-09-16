2813e25739f4911e8235a28a8e4847f995fbe509
Alan Lu <alanlu1023@gmail.com>
2020-09-16 17:50:20 -0500

    Add networks to Truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,36 @@
 module.exports = {
+  networks: {
+    mainnet: {
+      host: "localhost",
+      port: 8545,
+      network_id: "1"
+    },
+    ropsten: {
+      host: "localhost",
+      port: 8545,
+      network_id: "3"
+    },
+    kovan: {
+      host: "localhost",
+      port: 8545,
+      network_id: "42"
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      network_id: "4"
+    },
+    xdai: {
+      host: "localhost",
+      port: 8545,
+      network_id: "100"
+    },
+    local: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*"
+    }
+  },
   compilers: {
     solc: {
       version: "0.6.12",
